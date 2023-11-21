f09b81fd1199d30002b8b07199b13f24c9c2d8d0
Alexandru Tudose <xtudose@gmail.com>
2023-11-21 15:20:21 +0200

    add getJokeByTitle

--- chuck/src/app/joke-details/joke-details.service.ts
+++ chuck/src/app/joke-details/joke-details.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Joke } from '../models/joke.model';
+import { Observable, ReplaySubject, filter, first, from, of } from 'rxjs';
 
 // @Injectable({
 //     providedIn: 'root'
@@ -24,5 +25,28 @@ export class JokeDetailsService {
 
   addJoke(joke: Joke) {
     this._jokes.push(joke);
+  }
+
+  getJokeByTitle(title: string): Joke | undefined {
+    return this._jokes.filter(joke => joke.title === title)[0];
+  }
+
+
+  getJokeByTitle1(title: string): Observable<Joke | undefined> {
+    return of(this.getJokeByTitle(title));
+  }
+
+  getJokeByTitle2(title: string): Observable<Joke | undefined> {
+    return from(this._jokes).pipe(first(joke => joke.title === title));
+  }
+
+  getJokeByTitle3(title: string): Observable<Joke | undefined> {
+    return this.getJokes().pipe(first(joke => joke.title === title));
+  }
+
+  getJokes(): Observable<Joke> {
+    let subject = new ReplaySubject<Joke>();
+    this._jokes.forEach(joke => subject.next(joke));
+    return subject.asObservable();
   }
 }
